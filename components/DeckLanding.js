bd290868474a02c4f9023368c0a65d2cdc7383a0
madmaxatwork <tejasjukar@gmail.com>
2019-05-10 01:36:42 -0700

    Basic setup for helpers and LandingPage component

---
+++ components/DeckLanding.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Slider, StyleSheet, Text } from 'react-native'
+
+class DeckLanding extends React.Component {
+    render() {
+        return (
+          <View>
+            <Text>Displaying the Landing Page</Text>
+          </View>
+        );
+      }
+}
+
+export default DeckLanding;
