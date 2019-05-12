b4f132f2d8ef58bbeeff2441784b141e2f434817
madmaxatwork <tejasjukar@gmail.com>
2019-05-12 15:03:40 -0700

    UI for Add New Deck

--- components/AddNewDeck.js
+++ components/AddNewDeck.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { white, lightGray } from '../utils/colors'
 
 class AddNewDeck extends React.Component {
@@ -11,23 +11,39 @@ class AddNewDeck extends React.Component {
         return (
             <View style={styles.container}>
                 <Text style={styles.textStyle}>What is the title of your new deck?</Text>
+                <TextInput style={styles.deckTitleInput} underlineColorAndroid={'transparent'} editable={true} maxLength={100} placeholder="Deck Title"/>
             </View >
         );
     }
 }
 
+const width = '80%';
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 20,
         backgroundColor: white,
         alignItems: 'center',
         justifyContent: 'center',
     },
     textStyle: {
+        textAlign: 'center',
         color: lightGray,
         fontWeight: 'bold',
         fontSize: 20,
     },
+    deckTitleInput: {
+        marginTop: 10,
+        marginBottom: 10,
+        padding: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderWidth: 1,
+        borderColor: lightGray,
+        borderRadius: 4,
+        width
+      }
 });
 
 export default (AddNewDeck)
